a14758881d87164ebeb42583b1fe58da596c1c63
Travis Nicholson <110001213+Travisnicholson90@users.noreply.github.com>
2023-04-27 16:40:38 +1000

    last-working-stage-index.js

---
+++ helpers/uuid.js
@@ -0,0 +1,5 @@
+module.exports = () => 
+Math.random().toString()
+
+
+
--- routes/notes.js
+++ routes/notes.js
@@ -1,13 +1,51 @@
+const fs = require('fs');
 const path = require('path');
 const express = require('express');
 const router = express.Router();
 
-router.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/notes.html'));
+
+router.get('/api/notes', (req, res) => {
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+        } else {
+            const parsedData = JSON.parse(data);
+            console.log('parsedData from GET', parsedData);
+            res.json(parsedData);
+        }
+    });
 });
 
 router.post('/api/notes', (req, res) => {
-    console.log(req.body);
-})
+    console.info(`${req.method} request received to add a note`);
+
+    const { title, text } = req.body;
+
+    let newNote;
+    if(req.body) {
+        newNote = {
+            title,
+            text,
+            id: Math.floor(Math.random() * 1000),
+        }
+        console.log(newNote);
+    }
+
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+        } else {
+            const parsedData = JSON.parse(data);
+            parsedData.push(newNote);
+            fs.writeFile('./db/db.json', JSON.stringify(parsedData), (err) => {
+                if (err) {
+                    console.error(err);
+                } else {
+                    console.log('Note added!');
+                }
+            });
+        }
+    });
+});
 
 module.exports = router;
